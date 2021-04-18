ca60b58a1064020c15c7f98324aea388804cf00f
marijamilanovic <marijamil@uns.ac.rs>
2021-04-18 23:45:29 +0200

    [Frontend] init form

---
+++ frontend/my-app/src/App.vue
@@ -0,0 +1,90 @@
+<template>
+  <div id="app">
+    <!--<img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>-->
+    <h1>Nistagram</h1>
+    <br><br>
+    <b-form @submit="onSubmit" @reset="onReset" v-if="show">
+      <b-form-group id="input-group-1">
+        <b-form-input
+          id="email"
+          v-model="form.email"
+          type="email"
+          placeholder="Enter email"
+          required
+        ></b-form-input>
+      </b-form-group>
+
+      <b-form-input type="password" id="password" aria-describedby="password-help-block" placeholder="Enter password"></b-form-input>
+      <b-form-text id="password-help-block">
+        Your password must be 8-20 characters long, contain letters and numbers, and must not
+        contain spaces or special characters.
+      </b-form-text>
+
+      <br>
+      <b-button type="submit" variant="primary" style="width:200px;">Log in</b-button>
+      <br>
+    </b-form>
+
+    <hr>
+    <b-link href="#foo" style="font-size:15px">Don't have an account? Sign up</b-link>
+    <br>
+    <b-link href="#foo" style="font-size:15px">Forgot password?</b-link>
+  </div>
+  
+</template>
+
+<script>
+/*import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}*/
+export default {
+    data() {
+      return {
+        form: {
+          email: '',
+          name: '',
+        },
+        show: true
+      }
+    },
+    methods: {
+      onSubmit(event) {
+        event.preventDefault()
+        alert(JSON.stringify(this.form))
+      },
+      onReset(event) {
+        event.preventDefault()
+        // Reset our form values
+        this.form.email = ''
+        this.form.name = ''
+        // Trick to reset/clear native browser form validation state
+        this.show = false
+        this.$nextTick(() => {
+          this.show = true
+        })
+      }
+    }
+  }
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin: auto;
+  margin-top: 60px;
+  width: 25%;
+  border: 3px solid lightblue;
+  padding: 10px;
+}
+
+</style>
