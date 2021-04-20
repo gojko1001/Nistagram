75b891d773b4851a8393ae44514e6edabf073754
Gojko <gojko.novcic2@gmail.com>
2021-04-20 19:31:39 +0200

    [Frontend] Added logo

--- frontend/my-app/src/components/Navbar.vue
+++ frontend/my-app/src/components/Navbar.vue
@@ -2,8 +2,9 @@
   <div id="navbarPage">
     <nav class="navbar navbar-expand navbar-light" style="background-color: #e3f2fd;">
     <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
-        <a class="navbar-brand" href="#">
-            <img src="../assets/logo.png" width="40" height="40" alt="" />
+        <a class="navbar-brand" href="/">
+            <img src="../assets/logo.jpg"  height="40" alt="" />
+            <span class="brandName">Nistagram</span>
         </a>
         <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
             <li class="nav-item">
@@ -23,7 +24,6 @@
               <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
               <button class="btn btn-outline-secondary my-2 my-sm-0" type="submit">Search</button>
         </form>
-        
     </div>
     </nav>
   </div>
@@ -47,5 +47,12 @@ export default {
 <style>
 .nav-item{
   font-size: 20px;
+}
+.brandName{
+  font-family:Apple Chancery, cursive;
+  font-weight: 800;
+  font-size: 25px;
+  color:rgb(66, 87, 204);
+  margin-left: 10px;
 }
 </style>
