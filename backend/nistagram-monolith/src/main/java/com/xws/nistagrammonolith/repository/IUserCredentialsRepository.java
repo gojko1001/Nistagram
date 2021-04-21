08d2262b4b23d3cd3e120352f2b56aaa01f10464
marijamilanovic <marijamil@uns.ac.rs>
2021-04-21 16:41:23 +0200

    [Init] connected to database

--- backend/nistagram-monolith/src/main/java/com/xws/nistagrammonolith/repository/IUserCredentialsRepository.java
+++ backend/nistagram-monolith/src/main/java/com/xws/nistagrammonolith/repository/IUserCredentialsRepository.java
@@ -1,7 +1,9 @@
 package com.xws.nistagrammonolith.repository;
 
+import com.xws.nistagrammonolith.domain.UserCredentials;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface IUserCredentialsRepository {
+public interface IUserCredentialsRepository extends JpaRepository<UserCredentials, Long> {
 }
