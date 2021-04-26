a97d4715443e1b596fa836a8f6ae5a273a277e3f
tasa1998 <tasa1998@uns.ac.rs>
2021-04-26 17:22:22 +0200

    [Database] added import.sql

---
+++ backend/nistagram-monolith/src/main/resources/import.sql
@@ -0,0 +1,21 @@
+insert into blacklist (password) values ("123456");
+insert into blacklist (password) values ("123456789");
+insert into blacklist (password) values ("picture1");
+insert into blacklist (password) values ("password");
+insert into blacklist (password) values ("12345678");
+insert into blacklist (password) values ("111111");
+insert into blacklist (password) values ("123123");
+insert into blacklist (password) values ("12345");
+insert into blacklist (password) values ("1234567890");
+insert into blacklist (password) values ("senha");
+insert into blacklist (password) values ("1234567");
+insert into blacklist (password) values ("qwerty");
+insert into blacklist (password) values ("abc123");
+insert into blacklist (password) values ("Million2");
+insert into blacklist (password) values ("000000");
+insert into blacklist (password) values ("1234");
+insert into blacklist (password) values ("iloveyou");
+insert into blacklist (password) values ("aaron431");
+insert into blacklist (password) values ("password1");
+insert into blacklist (password) values ("qqww1122");
+
