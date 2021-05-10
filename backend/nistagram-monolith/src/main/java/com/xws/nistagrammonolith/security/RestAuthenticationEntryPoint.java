5fa47c6288877181dc33655454241025d5803e5f
Gojko <gojko.novcic2@gmail.com>
2021-05-10 19:36:52 +0200

    [Bugfix] Exceptions fix

--- backend/nistagram-monolith/src/main/java/com/xws/nistagrammonolith/security/RestAuthenticationEntryPoint.java
+++ backend/nistagram-monolith/src/main/java/com/xws/nistagrammonolith/security/RestAuthenticationEntryPoint.java
@@ -1,8 +1,11 @@
 package com.xws.nistagrammonolith.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerExceptionResolver;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
@@ -11,8 +14,13 @@ import java.io.IOException;
 
 @Component
 public class RestAuthenticationEntryPoint implements AuthenticationEntryPoint {
+//    @Autowired        //Uncomment to get real exceptions, swap lines in commence method 
+//    @Qualifier("handlerExceptionResolver")
+//    private HandlerExceptionResolver resolver;
+
     @Override
-        public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException e) throws IOException, ServletException {
+    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException e) throws IOException, ServletException {
         response.sendError(HttpServletResponse.SC_UNAUTHORIZED, e.getMessage());
+//        resolver.resolveException(request, response, null, e);
     }
 }
