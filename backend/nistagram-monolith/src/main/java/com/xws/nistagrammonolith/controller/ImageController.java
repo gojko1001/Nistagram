0a13cb679995a434d02427877bb5aae125f498a9
marijamilanovic <marijamil@uns.ac.rs>
2021-05-17 13:49:47 +0200

    [CleanCode] modified ImageService

--- backend/nistagram-monolith/src/main/java/com/xws/nistagrammonolith/controller/ImageController.java
+++ backend/nistagram-monolith/src/main/java/com/xws/nistagrammonolith/controller/ImageController.java
@@ -1,16 +1,13 @@
 package com.xws.nistagrammonolith.controller;
 
-import com.xws.nistagrammonolith.controller.dto.ImageBytesDto;
 import com.xws.nistagrammonolith.controller.dto.ImageDto;
 import com.xws.nistagrammonolith.domain.Image;
-import com.xws.nistagrammonolith.domain.Location;
 import com.xws.nistagrammonolith.repository.IImageRepository;
 import com.xws.nistagrammonolith.service.FileUploadUtil;
 import com.xws.nistagrammonolith.service.interfaces.IImageService;
 import com.xws.nistagrammonolith.service.interfaces.ILocationService;
 import com.xws.nistagrammonolith.service.interfaces.ITagService;
 import com.xws.nistagrammonolith.service.interfaces.IUserService;
-import org.apache.commons.io.IOUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -18,10 +15,7 @@ import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.File;
-import java.io.FileInputStream;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -33,12 +27,6 @@ public class ImageController {
     private IImageRepository imageRepository;
     @Autowired
     private IImageService imageService;
-    @Autowired
-    private ITagService tagService;
-    @Autowired
-    private ILocationService locationService;
-    @Autowired
-    private IUserService userService;
 
     private static String uploadDir = "user-photos";
 
@@ -50,69 +38,26 @@ public class ImageController {
         return fileName;
     }
 
-    //TODO: prebaciti u ImageService
+
     @PostMapping("/info")
     public Image saveImageInfo(@RequestBody ImageDto imageDto){
-        Image image = new Image();
-        image.setFileName(imageDto.getFileName());
-        image.setUsername(imageDto.getUsername());
-        image.setDescription(imageDto.getDescription());
-        Location location = locationService.findByName(imageDto.getLocationName());
-        image.setLocation(location);
-        image.setTags(tagService.createTags(imageDto.getTags()));
-        return imageService.create(image);
+        return imageService.saveImageInfo(imageDto);
     }
+
 
     @GetMapping("/profile/{username}")
     public ResponseEntity getImagesByUsername(@PathVariable("username") String username){
         List<Image> userImages = imageRepository.findImagesByUsername(username);
-        List<ImageBytesDto> imageBytesDtos = new ArrayList<>();
-        String filePath = new File("").getAbsolutePath();
-        filePath = filePath.concat("/" + uploadDir + "/");
-        for(Image image: userImages){
-            ImageBytesDto temp = new ImageBytesDto();
-            temp.setId(image.getId());
-            temp.setUsername(image.getUsername());
-            temp.setDescription(image.getDescription());
-            temp.setLocation(image.getLocation());
-            temp.setTags(image.getTags());
-            temp.setImageBytes(new ArrayList<>());
-            File in = new File(filePath + image.getFileName());
-            try {
-                temp.getImageBytes().add(IOUtils.toByteArray(new FileInputStream(in)));
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            imageBytesDtos.add(temp);
-        }
-        return new ResponseEntity(imageBytesDtos, HttpStatus.OK);
+        return new ResponseEntity(imageService.intoImageBytesDto(userImages), HttpStatus.OK);
     }
+
 
     @GetMapping("/discover/{username}")
     public ResponseEntity getDiscoverImages(@PathVariable("username") String username){
         List<Image> discoverImages = imageRepository.findAll();
         // TODO: provera da l je profil public i da l se prate ili je u pitanju gost
         // TODO: clean code
-        List<ImageBytesDto> imageBytesDtos = new ArrayList<>();
-        String filePath = new File("").getAbsolutePath();
-        filePath = filePath.concat("/" + uploadDir + "/");
-        for(Image image: discoverImages){
-            ImageBytesDto temp = new ImageBytesDto();
-            temp.setId(image.getId());
-            temp.setUsername(image.getUsername());
-            temp.setDescription(image.getDescription());
-            temp.setLocation(image.getLocation());
-            temp.setTags(image.getTags());
-            temp.setImageBytes(new ArrayList<>());
-            File in = new File(filePath + image.getFileName());
-            try {
-                temp.getImageBytes().add(IOUtils.toByteArray(new FileInputStream(in)));
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            imageBytesDtos.add(temp);
-        }
-        return new ResponseEntity(imageBytesDtos, HttpStatus.OK);
+        return new ResponseEntity(imageService.intoImageBytesDto(discoverImages), HttpStatus.OK);
     }
 
 }
--- backend/nistagram-monolith/src/main/java/com/xws/nistagrammonolith/service/ImageService.java
+++ backend/nistagram-monolith/src/main/java/com/xws/nistagrammonolith/service/ImageService.java
@@ -1,11 +1,21 @@
 package com.xws.nistagrammonolith.service;
 
+import com.xws.nistagrammonolith.controller.dto.ImageBytesDto;
+import com.xws.nistagrammonolith.controller.dto.ImageDto;
 import com.xws.nistagrammonolith.domain.Image;
+import com.xws.nistagrammonolith.domain.Location;
 import com.xws.nistagrammonolith.repository.IImageRepository;
 import com.xws.nistagrammonolith.service.interfaces.IImageService;
+import com.xws.nistagrammonolith.service.interfaces.ILocationService;
+import com.xws.nistagrammonolith.service.interfaces.ITagService;
+import org.apache.commons.io.IOUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -13,6 +23,12 @@ public class ImageService implements IImageService {
 
     @Autowired
     private IImageRepository imageRepository;
+    @Autowired
+    private ILocationService locationService;
+    @Autowired
+    private ITagService tagService;
+
+    private static String uploadDir = "user-photos";
 
     @Override
     public List<Image> getAll() {
@@ -26,5 +42,43 @@ public class ImageService implements IImageService {
 
     @Override
     public List<Image> getUserImages(String username){ return imageRepository.findImagesByUsername(username);}
+
+    @Override
+    public Image saveImageInfo(ImageDto imageDto){
+        Image image = new Image();
+        image.setFileName(imageDto.getFileName());
+        image.setUsername(imageDto.getUsername());
+        image.setDescription(imageDto.getDescription());
+        Location location = locationService.findByName(imageDto.getLocationName());
+        image.setLocation(location);
+        image.setTags(tagService.createTags(imageDto.getTags()));
+        return create(image);
+    }
+
+    @Override
+    public List<ImageBytesDto> intoImageBytesDto(List<Image> images){
+        List<ImageBytesDto> imageBytesDtos = new ArrayList<>();
+        if(images != null){
+            String filePath = new File("").getAbsolutePath();
+            filePath = filePath.concat("/" + uploadDir + "/");
+            for(Image image: images){
+                ImageBytesDto temp = new ImageBytesDto();
+                temp.setId(image.getId());
+                temp.setUsername(image.getUsername());
+                temp.setDescription(image.getDescription());
+                temp.setLocation(image.getLocation());
+                temp.setTags(image.getTags());
+                temp.setImageBytes(new ArrayList<>());
+                File in = new File(filePath + image.getFileName());
+                try {
+                    temp.getImageBytes().add(IOUtils.toByteArray(new FileInputStream(in)));
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                imageBytesDtos.add(temp);
+            }
+        }
+        return imageBytesDtos;
+    }
 
 }
