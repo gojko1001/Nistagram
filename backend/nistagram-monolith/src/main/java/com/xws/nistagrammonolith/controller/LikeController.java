77d9981b0b22e863eac939cfa64e4596859f3353
marijamilanovic <marijamil@uns.ac.rs>
2021-06-01 20:13:20 +0200

    [BugFix]

--- backend/nistagram-monolith/src/main/java/com/xws/nistagrammonolith/controller/LikeController.java
+++ backend/nistagram-monolith/src/main/java/com/xws/nistagrammonolith/controller/LikeController.java
@@ -6,6 +6,8 @@ import com.xws.nistagrammonolith.service.interfaces.ILikeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/like")
 @CrossOrigin(origins = "${clientURL}")
