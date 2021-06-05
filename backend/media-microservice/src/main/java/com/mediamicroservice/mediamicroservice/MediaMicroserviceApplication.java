d3ec3e969d98c817bbee538532f2d699757e08db
majastamenic <stamenicmaja5@uns.ac.rs>
2021-06-05 19:13:11 +0200

    [Zuul]

--- backend/media-microservice/src/main/java/com/mediamicroservice/mediamicroservice/MediaMicroserviceApplication.java
+++ backend/media-microservice/src/main/java/com/mediamicroservice/mediamicroservice/MediaMicroserviceApplication.java
@@ -3,10 +3,16 @@ package com.mediamicroservice.mediamicroservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 @EnableEurekaClient
 public class MediaMicroserviceApplication {
+
+	@GetMapping("/media")
+	public String getMediaMicroservice(){
+		return "Media microservice called.";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(MediaMicroserviceApplication.class, args);
