4004d577510ce3248de5d30b8f784809bf1cc735
majastamenic <stamenicmaja5@uns.ac.rs>
2021-06-06 11:52:01 +0200

    [Zuul]

--- backend/user-microservice/src/main/java/com/nistagram/usermicroservice/UserMicroserviceApplication.java
+++ backend/user-microservice/src/main/java/com/nistagram/usermicroservice/UserMicroserviceApplication.java
@@ -3,10 +3,18 @@ package com.nistagram.usermicroservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableEurekaClient
+@RestController
 public class UserMicroserviceApplication {
+
+	@GetMapping
+	public String getMediaMicroservice(){
+		return "User microservice called.";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(UserMicroserviceApplication.class, args);
