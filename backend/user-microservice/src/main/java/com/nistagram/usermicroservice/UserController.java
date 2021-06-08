d74cd75b17f5bd556542aa948626048491a5f9d5
marijamilanovic <marijamil@uns.ac.rs>
2021-06-08 13:10:59 +0200

    [BugFix] fixed running microservices in Docker

--- backend/user-microservice/src/main/java/com/nistagram/usermicroservice/UserController.java
+++ backend/user-microservice/src/main/java/com/nistagram/usermicroservice/UserController.java
@@ -14,7 +14,7 @@ import java.util.List;
 @Slf4j
 @RestController
 @RequestMapping("api/user")
-@CrossOrigin(origins = "${gatewayURL}")
+//@CrossOrigin(origins = "${gatewayURL}")
 public class UserController {
 
     @Autowired
