5b9c25982041d9a48059fc1d5843f7b5a9d37d8d
majastamenic <stamenicmaja5@uns.ac.rs>
2021-06-10 11:05:16 +0200

    [BugFixed] Dependency check

--- backend/user-microservice/src/main/java/com/nistagram/usermicroservice/UserMicroserviceApplication.java
+++ backend/user-microservice/src/main/java/com/nistagram/usermicroservice/UserMicroserviceApplication.java
@@ -3,10 +3,8 @@ package com.nistagram.usermicroservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
 @EnableEurekaClient
 public class UserMicroserviceApplication {
 
