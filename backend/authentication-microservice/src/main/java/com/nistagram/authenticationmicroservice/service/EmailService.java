bf29da5ab090e6dbb5cca6da06f7e7bae8d2a661
tasa1998 <tasa1998@uns.ac.rs>
2021-06-10 23:07:10 +0200

    [Merge] merge with develop

---
+++ backend/authentication-microservice/src/main/java/com/nistagram/authenticationmicroservice/service/EmailService.java
@@ -0,0 +1,60 @@
+package com.xws.nistagrammonolith.service;
+
+import com.xws.nistagrammonolith.domain.User;
+import com.xws.nistagrammonolith.security.JwtService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.MailException;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailService {
+
+    @Autowired
+    private JavaMailSender javaMailSender;
+
+    @Value("${spring.mail.username}")
+    private String mailSender;
+
+    private String verifyUrl = "https://localhost:3030/userCredentials/verify/";
+    @Autowired
+    private JwtService jwtService;
+
+    @Async
+    public void verificationPassword(User user) throws MailException {
+        String jwt = jwtService.createToken(user.getUsername()/*, Role.ROLE_USER*/);
+        SimpleMailMessage simpleMailMessage = new SimpleMailMessage();
+        simpleMailMessage.setFrom(mailSender);
+        simpleMailMessage.setTo(user.getEmail());
+        simpleMailMessage.setSubject("Registration");
+        String url = verifyUrl + jwt;
+        String mailText = "Dear " + user.getFullName() + ",\n\n" +
+                "Welcome to Ništagram \n\n" +
+                "Your username is " + user.getUsername() + "\n\n" +
+                "You can verify your account here:" + "\t" +
+                "<a href=\"" + url + "\">Verify your account</a>" + "\n\n" +
+                "Best regards.";
+        simpleMailMessage.setText(mailText);
+        javaMailSender.send(simpleMailMessage);
+    }
+
+    @Async
+    public void resetPassword(User user) throws MailException {
+        String jwt = jwtService.createToken(user.getUsername()/*, Role.ROLE_USER*/);
+        String url = "https://localhost:3000/reset_password/" + jwt;
+        SimpleMailMessage simpleMailMessage = new SimpleMailMessage();
+        simpleMailMessage.setFrom(mailSender);
+        simpleMailMessage.setTo(user.getEmail());
+        simpleMailMessage.setSubject("Restart password");
+        String mailText = "Dear " + user.getFullName() + ",\n\n" +
+                "Welcome to Ništagram" + "\n\n" +
+                "<a href=\"" + url + "\">Reset password</a>" + "\n\n" +
+                "Best regards,";
+        simpleMailMessage.setText(mailText);
+        javaMailSender.send(simpleMailMessage);
+    }
+
+}
