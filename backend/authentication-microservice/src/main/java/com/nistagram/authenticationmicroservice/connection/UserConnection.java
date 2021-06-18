5a65437898df6366055bc5f0bd8805b00fad77b8
majastamenic <stamenicmaja5@uns.ac.rs>
2021-06-18 13:31:54 +0200

    [Feature] RestTemplate

--- backend/authentication-microservice/src/main/java/com/nistagram/authenticationmicroservice/connection/UserConnection.java
+++ backend/authentication-microservice/src/main/java/com/nistagram/authenticationmicroservice/connection/UserConnection.java
@@ -2,19 +2,73 @@ package com.nistagram.authenticationmicroservice.connection;
 
 import com.nistagram.authenticationmicroservice.dto.UserCredentialsDto;
 import com.nistagram.authenticationmicroservice.dto.UserDto;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.client.RestTemplate;
 
-@FeignClient(value = "user-microservice", url = "http://localhost:3032")
-public interface UserConnection {
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
-    @PostMapping("/user/add")
-    void registerUser(@RequestBody UserCredentialsDto userReg);
+//@FeignClient(value = "user-microservice", url = "http://localhost:3032")
+//public interface UserConnection {
+//
+//    @PostMapping("/user/add")
+//    void registerUser(@RequestBody UserCredentialsDto userReg);
+//
+//    @PostMapping("/user/addGoogleUser")
+//    void registerGoogleUser(@RequestBody UserCredentialsDto userReg);
+//
+//    @PostMapping("/user/find")
+//    UserDto getUserByEmail(@RequestBody String email);
+//}
+@Service
+public class UserConnection {
 
-    @PostMapping("/user/addGoogleUser")
-    void registerGoogleUser(@RequestBody UserCredentialsDto userReg);
+    @Autowired
+    private RestTemplate restTemplate;
 
-    @PostMapping("/user/find")
-    UserDto getUserByEmail(@RequestBody String email);
+    public void registerUser(UserCredentialsDto userReq) {
+        String url = "http://localhost:8762/user-api/user/add";
+        try {
+            restTemplate.exchange(url, HttpMethod.POST,
+                    new HttpEntity<>(userReq, new HttpHeaders()), Void.class, 1);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void registerGoogleUser(UserCredentialsDto userReq) {
+        String url = "http://localhost:8762/user-api/user/addGoogleUser";
+        try {
+            restTemplate.exchange(url, HttpMethod.POST,
+                    new HttpEntity<>(userReq, new HttpHeaders()), Void.class, 1);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public UserDto getUserByEmail(String email) {
+        String url = "http://localhost:8762/user-api/user/find";
+        UserDto userDto = new UserDto();
+        try {
+            ResponseEntity<UserDto> response = restTemplate.exchange(url, HttpMethod.POST,
+                    new HttpEntity<>(email, new HttpHeaders()), UserDto.class, 1);
+            userDto = response.getBody();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return userDto;
+    }
+
 }
--- backend/media-microservice/src/main/java/com/mediamicroservice/mediamicroservice/connection/AuthConnection.java
+++ backend/media-microservice/src/main/java/com/mediamicroservice/mediamicroservice/connection/AuthConnection.java
@@ -1,12 +1,39 @@
 package com.mediamicroservice.mediamicroservice.connection;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.client.RestTemplate;
 
-@FeignClient(value = "authentication-microservice", url = "http://localhost:3031")
-public interface AuthConnection {
+@Service
+public class AuthConnection {
 
-    @GetMapping("/verify/{username}")
-    String verifyUser(@PathVariable String username);
+    @Autowired
+    private RestTemplate restTemplate;
+
+    public String verifyUser(String username) {
+        String url = "http://localhost:8762/user-api/verify/" + username;
+        String text = "";
+        try {
+            ResponseEntity<String> response = restTemplate.getForEntity(url, String.class);
+            text = response.getBody();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return text;
+    }
 }
+
+//@FeignClient(value = "authentication-microservice", url = "http://localhost:3031")
+//public interface AuthConnection {
+//
+//    @GetMapping("/verify/{username}")
+//    String verifyUser(@PathVariable String username);
+//}
