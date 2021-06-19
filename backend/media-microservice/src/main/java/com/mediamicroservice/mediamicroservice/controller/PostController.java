ed5e684445e89764250c8135fdfbda8e7f928154
marijamilanovic <marijamil@uns.ac.rs>
2021-06-19 15:41:04 +0200

    [FeatureUpdate] modified method for getting posts on discover page

--- backend/media-microservice/src/main/java/com/mediamicroservice/mediamicroservice/controller/PostController.java
+++ backend/media-microservice/src/main/java/com/mediamicroservice/mediamicroservice/controller/PostController.java
@@ -60,11 +60,7 @@ public class PostController {
 
     @GetMapping("/discover/{username}")
     public ResponseEntity getDiscoverImages(@PathVariable("username") String username) {
-        //Logger.info("Get discover images.", username);
-        List<Post> discoverPosts = postService.getPublicPosts();
-        // TODO: provera da l je profil public i da l se prate ili je u pitanju gost
-        // TODO: clean code
-        return new ResponseEntity(postService.getImagesFiles(discoverPosts), HttpStatus.OK);
+        return postService.getDiscoverImages(username);
     }
 
     @GetMapping("/{id}")
--- backend/media-microservice/src/main/java/com/mediamicroservice/mediamicroservice/service/PostService.java
+++ backend/media-microservice/src/main/java/com/mediamicroservice/mediamicroservice/service/PostService.java
@@ -37,7 +37,7 @@ public class PostService implements IPostService {
     @Autowired
     private UserConnection userConnection;
     @Autowired
-    private IMediaNameRepository mediaNameRepository;
+    private IMediaNameService mediaNameService;
     @Autowired
     private IInappropriateContentService inappropriateContentService;
     @Autowired
@@ -99,7 +99,7 @@ public class PostService implements IPostService {
             if (fileName.contains(".mp4")) {
                 mediaName.setImage(false);
             }
-            mediaNameRepository.save(mediaName);
+            mediaNameService.save(mediaName);
             mediaNames.add(mediaName);
         }
         media.setUsername(imageDto.getUsername());
@@ -254,6 +254,26 @@ public class PostService implements IPostService {
         Set<Post> set = new HashSet<>(publicPosts);
         publicPosts = set.stream().collect(Collectors.toList());
         return publicPosts;
+    }
+
+    @Override
+    public ResponseEntity getDiscoverImages(String username){
+        List<Post> discoverPosts = new ArrayList<>();
+        if(username.equals("null")){  // for guests
+            discoverPosts = getPublicPosts();
+        }else{                      // for logged user
+            List<String> usernames = userTagService.getUserTagsByUsername(username);
+            usernames.add(username);
+            for(String u : usernames){
+                List<Post> posts = postRepository.findPostsByMedia_Username(u);
+                if(posts != null){
+                    for(Post p : posts){
+                        discoverPosts.add(p);
+                    }
+                }
+            }
+        }
+        return new ResponseEntity(getImagesFiles(discoverPosts), HttpStatus.OK);
     }
 
 }
