3ac7dc26da6a4bd21f95016394ab5d3b0a92a6a9
marijamilanovic <marijamil@uns.ac.rs>
2021-06-20 15:28:59 +0200

    [Docker]

--- backend/media-microservice/src/main/java/com/mediamicroservice/mediamicroservice/service/StoryService.java
+++ backend/media-microservice/src/main/java/com/mediamicroservice/mediamicroservice/service/StoryService.java
@@ -137,7 +137,7 @@ public class StoryService implements IStoryService {
         List<StoryBytesDto> storyBytesDto = new ArrayList<>();
         if (stories != null) {
             String filePath = new File("").getAbsolutePath();
-            filePath = filePath.concat("/src/" + uploadDir + "/");
+            filePath = filePath.concat("/src/main/resources/" + uploadDir + "/");
             for (Story story : stories) {
                 for(MediaName mn : story.getMedia().getMediaName()){
                     if(!isReported(story.getMedia().getId()))
